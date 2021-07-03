826fe9d88719977bf0a7f890f2ddc4cc99a953ff
Gilbert Ng'eno <ngenogilbert07@gmail.com>
2021-07-03 19:08:04 +0300

    Initial commit

--- src/Util/data.js
+++ src/Util/data.js
@@ -1,186 +1,196 @@
-export const FULL_NAME = 'Yash Walia'
-export const RESUME_LINK = 'https://drive.google.com/file/d/1DjRJCnkOuMAP7Edoggn3gYkMhac-7wZ-/view?usp=sharing'
-export const FOOTER_TEXT = "Built by Yash Walia";
+export const FULL_NAME = "Gilbert Ngeno";
+export const RESUME_LINK =
+  "https://drive.google.com/file/d/1Vuxs_VPQmfSJKtjqno5bCkqGopWsMxhE/view?usp=sharing";
+export const FOOTER_TEXT = "Built by Gilbert Ngeno";
 export const SOCIAL_LINKS = [
-    {
-        "name": "Email",
-        "icon": "far fa-lg fa-envelope",
-        "link": "mailto:yashbest005@gmail.com"
-    },
-    {
-        "name": "Github",
-        "icon": "fab fa-lg fa-github",
-        "link": "https://github.com/ywalia01"
-    },
-    {
-        "name": "Linkedin",
-        "icon": "fab fa-lg fa-linkedin-in",
-        "link": "https://www.linkedin.com/in/yash-walia"
-    },
-    {
-        "name": "Discord",
-        "icon": "fab fa-lg fa-discord",
-        "link": "https://discord.com/users/623151272069169174"
-    },
-    {
-        "name": "Twitter",
-        "icon": "fab fa-lg fa-twitter",
-        "link": "https://twitter.com/ywalia01"
-    },
-    {
-        "name": "Instagram",
-        "icon": "fab fa-lg fa-instagram",
-        "link": "https://www.instagram.com/notyashwalia/"
-    }
-]
+  {
+    name: "Email",
+    icon: "far fa-lg fa-envelope",
+    link: "ngenogilbert07@gmail.com",
+  },
+  {
+    name: "Github",
+    icon: "fab fa-lg fa-github",
+    link: "https://github.com/gilly7",
+  },
+  {
+    name: "Linkedin",
+    icon: "fab fa-lg fa-linkedin-in",
+    link: "https://www.linkedin.com/in/gilbert-ngeno-b4290b143/",
+  },
+  {
+    name: "Reddit",
+    icon: "fab fa-lg fa-discord",
+    link: "https://reddit.com/Mfalme7",
+  },
+  {
+    name: "Twitter",
+    icon: "fab fa-lg fa-twitter",
+    link: "https://twitter.com/gilbert4_real",
+  },
+  {
+    name: "Google Developer Profile",
+    icon: "fab fa-lg fa-google",
+    link: "https://developers.google.com/profile/u/100004141949824415732",
+  },
+];
 
 export const SKILLS = [
-    {
-        "name": "Python",
-        "icon": "devicon-python-plain colored",
-    },
-    {
-        "name": "Javascript",
-        "icon": "devicon-javascript-plain colored",
-    },
-    {
-        "name": "C/C++",
-        "icon": "devicon-c-plain colored",
-    },
-    {
-        "name": "Java",
-        "icon": "devicon-java-plain colored",
-    },
-    {
-        "name": "Reactjs",
-        "icon": "devicon-react-original colored",
-    },
-    {
-        "name": "Typescript",
-        "icon": "devicon-typescript-plain colored",
-    },
-    {
-        "name": "Flask",
-        "svg": "https://user-images.githubusercontent.com/48391286/116382927-045e8080-a834-11eb-95bd-f2dacbd6b0f8.png",
-    },
-    {
-        "name": "Django",
-        "svg": "https://static.djangoproject.com/img/icon-touch.e4872c4da341.png",
-    },
-    {
-        "name": "Node",
-        "icon": "devicon-nodejs-plain colored",
-    },
-    {
-        "name": "MongoDB",
-        "icon": "devicon-mongodb-plain colored",
-    },
-    {
-        "name": "Git",
-        "icon": "devicon-git-plain colored",
-    },
-    {
-        "name": "Docker",
-        "icon": "devicon-docker-plain colored",
-    },
-    {
-        "name": "Bash",
-        "icon": "devicon-bash-plain colored",
-    },
-    {
-        "name": "Linux",
-        "svg": "https://user-images.githubusercontent.com/48391286/116357092-b63c8380-a819-11eb-8975-30668aa7d647.png",
-    },
-]
+  {
+    name: "Kotlin",
+    icon: "devicon-kotlin-plain colored",
+  },
+  {
+    name: "Android",
+    icon: "devicon-android-plain colored",
+  },
+  {
+    name: "C/C++",
+    icon: "devicon-c-plain colored",
+  },
+  {
+    name: "Java",
+    icon: "devicon-java-plain colored",
+  },
+  {
+    name: "Firebase",
+    icon: "devicon-firebase-plain",
+  },
+  {
+    name: "Reactjs",
+    icon: "devicon-react-original colored",
+  },
+  {
+    name: "Kubernetes",
+    icon: "devicon-kubernetes-plain colored",
+  },
+  {
+    name: "Flask",
+    svg:
+      "https://user-images.githubusercontent.com/48391286/116382927-045e8080-a834-11eb-95bd-f2dacbd6b0f8.png",
+  },
+
+  {
+    name: "Node",
+    icon: "devicon-nodejs-plain colored",
+  },
+  {
+    name: "MongoDB",
+    icon: "devicon-mongodb-plain colored",
+  },
+  {
+    name: "Git",
+    icon: "devicon-git-plain colored",
+  },
+  {
+    name: "Docker",
+    icon: "devicon-docker-plain colored",
+  },
+  {
+    name: "Bash",
+    icon: "devicon-bash-plain colored",
+  },
+  {
+    name: "Fedora",
+    svg:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Fedora_logo.svg/1200px-Fedora_logo.svg.png",
+  },
+];
 
 export const ABOUT = [
-    {
-        "index": "1",
-        "text": "I'm a 19 year-old student currently pursuing a bachelor's degree in Computer Science Engineering."
-    },
-    {
-        "index": "2",
-        "text": "I enjoy solving problems, writing and building products. I'm also passionate about communities and open-source."
-    },
-    {
-        "index": "3",
-        "text": "I like to peek under the hood, tinker with technologies and learn new skills."
-    },
-    {
-        "index": "4",
-        "text": "I'm an avid reader and generally curious."
-    }
-]
+  {
+    index: "1",
+    text:
+      "Passionate and curiosity-driven software engineer with an eye on android, its development, and emerging technologies. Ready to dive into writing clean code and building more accessible applications with great user experience.",
+  },
+  {
+    index: "2",
+    text:
+      "I enjoy solving problems, writing and building products. I'm also passionate about communities and open-source.",
+  },
+  {
+    index: "3",
+    text:
+      "I like to peek under the hood, tinker with technologies and learn new skills.",
+  },
+  {
+    index: "4",
+    text: "I'm an avid reader and generally curious.",
+  },
+];
 
 export const WORK = [
-    {
-        "title": "Dune API Landing Page",
-        "description": "Documentation for the Dune API",
-        "description2": "Reactjs",
-        "ghlink": "https://github.com/ywalia01/dune-api-frontend",
-        "extlink": "https://ywalia01.github.io/dune-api-frontend/"
-    },
-    {
-        "title": "Dune API",
-        "description": "A simple API which provides you with book, character, movie and quotes JSON data",
-        "description2": "Nodejs, Expressjs",
-        "ghlink": "https://github.com/ywalia01/dune-api",
-        "extlink": "https://the-dune-api.herokuapp.com/quotes/1"
-    },
-    {
-        "title": "random-dune-quotes",
-        "description": "A simple NPM package that serves random quotes from the Dune franchise",
-        "description2": "Nodejs, Expressjs",
-        "ghlink": "",
-        "extlink": "https://www.npmjs.com/package/random-dune-quotes"
-    },
-    {
-        "title": "Developer Portfolio",
-        "description": "A clean, minimal responsive portfolio for developers",
-        "description2": "Reactjs",
-        "ghlink": "https://github.com/ywalia01/ywalia01.github.io",
-        "extlink": "https://ywalia01.netlify.app/"
-    },
-    {
-        "title": "Artist Portfolio",
-        "description": "A responsive grid portfolio template for artists",
-        "description2": "Reactjs, Nodejs, Expressjs, MongoDB",
-        "ghlink": "https://github.com/ywalia01/artist-portfolio",
-        "extlink": "https://artist-pfolio.netlify.app/"
-    },
-    {
-        "title": "Jchain",
-        "description": "A simple blockchain simulation GUI application",
-        "description2": "Java, Swing",
-        "ghlink": "https://github.com/ywalia01/jchain",
-        "extlink": "https://github.com/ywalia01/jchain"
-    },
-    {
-        "title": "iHeard",
-        "description": "A web app designed for the hard of hearing",
-        "description2": "Machine Learning, Speech to text, Google Cloud API",
-        "ghlink": "https://github.com/ywalia01/iheard",
-        "extlink": "https://iheard.tech/"
-    },
-    {
-        "title": "Yelpcamp",
-        "description": "An implementation of the popular crowd-sourced review service provider - Yelp. It has basic CRUD Functionality",
-        "description2": "Nodejs, Expressjs, MongoDB",
-        "ghlink": "https://github.com/ywalia01/yelpcampv9",
-        "extlink": "https://github.com/ywalia01/yelpcampv9"
-    },
-    {
-        "title": "CountDune",
-        "description": "A simple countdown timer for Dune 2021",
-        "description2": "Vanilla Javascript",
-        "ghlink": "https://github.com/ywalia01/countdune",
-        "extlink": "https://ywalia01.github.io/countdune/"
-    },
-    {
-        "title": "Yodish Converter",
-        "description": "A simple API interface for English to Yodish conversion",
-        "description2": "Vanilla Javascript",
-        "ghlink": "https://github.com/ywalia01/yodish",
-        "extlink": "https://ywalia01.github.io/yodish/"
-    }
-]
+  {
+    title: "MeChat 254",
+    description:
+      "Using Firebase to easily create web applications by implementing and deploying a chat client using Firebase products and services.",
+    description2: "Firebase",
+    ghlink: "https://github.com/gilly7/gilly7-web-start",
+    extlink: "mechat.web.app",
+  },
+  {
+    title: "WhatsThe Weather",
+    description: "An App to find weather of major cities in the world",
+    description2: "Yelp API, JSON, Java",
+    ghlink: "https://github.com/gilly7/WhatsTheWeather",
+    extlink: "https://github.com/gilly7/WhatsTheWeather",
+  },
+  {
+    title: "random-dune-quotes",
+    description:
+      "A food delivery app developed with flutter with firebase authentication",
+    description2: "Nodejs, Expressjs",
+    ghlink: "https://github.com/gilly7/flutter_zone_fod_app",
+    extlink: "",
+  },
+  {
+    title: "Developer Portfolio",
+    description: "A clean, minimal responsive portfolio for developers",
+    description2: "Reactjs",
+    ghlink: "https://github.com/ywalia01/ywalia01.github.io",
+    extlink: "https://ywalia01.netlify.app/",
+  },
+  {
+    title: "Artist Portfolio",
+    description: "A responsive grid portfolio template for artists",
+    description2: "Reactjs, Nodejs, Expressjs, MongoDB",
+    ghlink: "https://github.com/ywalia01/artist-portfolio",
+    extlink: "https://artist-pfolio.netlify.app/",
+  },
+  {
+    title: "Jchain",
+    description: "A simple blockchain simulation GUI application",
+    description2: "Java, Swing",
+    ghlink: "https://github.com/ywalia01/jchain",
+    extlink: "https://github.com/ywalia01/jchain",
+  },
+  {
+    title: "iHeard",
+    description: "A web app designed for the hard of hearing",
+    description2: "Machine Learning, Speech to text, Google Cloud API",
+    ghlink: "https://github.com/ywalia01/iheard",
+    extlink: "https://iheard.tech/",
+  },
+  {
+    title: "Yelpcamp",
+    description:
+      "An implementation of the popular crowd-sourced review service provider - Yelp. It has basic CRUD Functionality",
+    description2: "Nodejs, Expressjs, MongoDB",
+    ghlink: "https://github.com/ywalia01/yelpcampv9",
+    extlink: "https://github.com/ywalia01/yelpcampv9",
+  },
+  {
+    title: "CountDune",
+    description: "A simple countdown timer for Dune 2021",
+    description2: "Vanilla Javascript",
+    ghlink: "https://github.com/ywalia01/countdune",
+    extlink: "https://ywalia01.github.io/countdune/",
+  },
+  {
+    title: "Yodish Converter",
+    description: "A simple API interface for English to Yodish conversion",
+    description2: "Vanilla Javascript",
+    ghlink: "https://github.com/ywalia01/yodish",
+    extlink: "https://ywalia01.github.io/yodish/",
+  },
+];
